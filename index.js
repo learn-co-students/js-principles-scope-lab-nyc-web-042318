d4549bd5eb7092dbf4b4ecddd040950dbfcf81d4
Canino Developments <ian.canino@gmail.com>
2018-06-02 14:52:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustome;
+const leastFavoriteCustomer = 'alice';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bill';
+}
+
